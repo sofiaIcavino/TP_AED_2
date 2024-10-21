a20e7187f17cd3df1f712c69743bd660b3b215d3
sofiaIcavino <sofiaicavino@gmail.com>
2024-10-20 22:56:54 -0400

    Campo Minado

--- campo_minado_Sofia_Pinho_Icavino_Moura.c
+++ campo_minado_Sofia_Pinho_Icavino_Moura.c
@@ -10,7 +10,7 @@
 #define BombasM 6;
 #define BombasD 9;
 
-/*Fazer alocação das matrizes inteiras de maneira dinâmica*/
+/*Fazer alocacao das matrizes inteiras de maneira dinamica*/
 int** alocar_matriz(int tamanho){
 	int i=0;
 
@@ -22,7 +22,7 @@ int** alocar_matriz(int tamanho){
    return matriz_alocada;
 }
 
-/*Fazer alocação e inicialização(com o caracter x) da matriz de vizualização do usuário*/
+/*Fazer alocacao e inicializacao(com o caracter x) da matriz de vizualizacao do usuario*/
 char** alocarEinicializar_matriz_usuario(int tamanho){
 	int i=0,j=0;
    char** matriz_alocada=(char**)malloc(tamanho*sizeof(char*));
@@ -40,7 +40,7 @@ char** alocarEinicializar_matriz_usuario(int tamanho){
    return matriz_alocada;
 }
 
-/*Função para inicialização das matrizes inteiras com 0*/
+/*Funcao para inicializacao das matrizes inteiras com 0*/
 void inicializar_matriz(int **matriz,int tamanho){
 	int i=0,j=0;
 
@@ -51,7 +51,7 @@ void inicializar_matriz(int **matriz,int tamanho){
    }
 }
 
-/*Função para realizar a impresão da matriz na tela*/
+/*Funcao para realizar a impressao da matriz na tela*/
 void visualizar_matriz(int **matriz,int tamanho){
 	int i=0,j=0;
 
@@ -67,12 +67,12 @@ void visualizar_matriz(int **matriz,int tamanho){
    }
 }
 
-/*Função para realizar a impresão da matriz de vizualização do usuário na tela,
+/*Funcao para realizar a impressao da matriz de vizualizacao do usuario na tela,
 de maneira que fique intuitiva para o jogador*/
 void visualizar_matriz_usuario(char **matriz,int tamanho){
 	int i=0,j=0;
 
-   //Impressão da coordenada Y - as colunas
+   //Impressao da coordenada Y - as colunas
    printf("    ");
    for(i=1;i<tamanho+1;i++){
       if(i>=9){
@@ -84,7 +84,7 @@ void visualizar_matriz_usuario(char **matriz,int tamanho){
    printf("\n");
 
    for(i=0;i<tamanho;i++){
-      //Impressão da coordenada X - as linhas
+      //Impressao da coordenada X - as linhas
       if(i>=9){
          printf(" %d ",i+1);
       }else{
@@ -92,7 +92,7 @@ void visualizar_matriz_usuario(char **matriz,int tamanho){
       }
   
       for( j=0;j<tamanho;j++){
-         //Impressão da matriz para o jogador realizar a escolha das coordenadas
+         //Impressao da matriz para o jogador realizar a escolha das coordenadas
          printf(" %c ", matriz[i][j]); 
       }
       printf("\n");
@@ -100,7 +100,7 @@ void visualizar_matriz_usuario(char **matriz,int tamanho){
 
 }
 
-/*Função responsável por configurar o jogo. Ao escolher a dificuldade do jogo, tam recebe 
+/*Funcao responsavel por configurar o jogo. Ao escolher a dificuldade do jogo, tam recebe 
 a dimensao da matriz e quantBombas recebe a quantidade de bombas de acordo com sua dificuldade*/
 void dificuldadeJogo(int *modo,int* tam,int *quantBomba){
    switch (*modo)
@@ -122,7 +122,7 @@ void dificuldadeJogo(int *modo,int* tam,int *quantBomba){
    }
 }
 
-/*Função para inserir as bombas de modo aleatório*/
+/*Funcao para inserir as bombas de modo aleatorio*/
 void inserirBombas(int quantBombas,int **matriz,int tamanho){
    int cont=0;
    srand(time(NULL));
@@ -131,11 +131,11 @@ void inserirBombas(int quantBombas,int **matriz,int tamanho){
       int linha= rand()%tamanho; //Gerar uma linha aleatoria
       int coluna= rand()%tamanho; //Gerar uma coluna aleatoria
 
-      /*O if irá verficar se já existe bomba no local escolhido, para que as bombas 
-      não sejam geradas no mesmo local.
-      Se na coordenada sorteada estiver o valor 0, não tem bomba. Assim, adicionando -1 no local
-      da bomba. Quando diferente de zero, quer dizer que há bomba no local e é gerado uma 
-      nova coordenada até atingir a quantidade certa de bombas*/
+      /*O if ira verficar se ja existe bomba no local escolhido, para que as bombas 
+      nao sejam geradas no mesmo local.
+      Se na coordenada sorteada estiver o valor 0, nao tem bomba. Assim, adicionando -1 no local
+      da bomba. Quando diferente de zero, quer dizer que ha bomba no local e eh gerado uma 
+      nova coordenada ate atingir a quantidade certa de bombas*/
       if(matriz[linha][coluna]==0){
         matriz[linha][coluna]=-1;
         cont++;
@@ -143,8 +143,8 @@ void inserirBombas(int quantBombas,int **matriz,int tamanho){
    }
 }
 
-/*Função verifica quantas bombas tem ao redor da coordenada. Verificando sempre as 8 coordenadas 
-que ficam na vizinhança, e se elas existem.*/
+/*Funcao verifica quantas bombas tem ao redor da coordenada. Verificando sempre as 8 coordenadas 
+que ficam na vizinhanca, e se elas existem.*/
 int colocarQuantidadeDeBombas(int **matriz, int linha, int coluna, int tamanho){ 
     int quantBombas = 0;
 
@@ -192,8 +192,8 @@ int colocarQuantidadeDeBombas(int **matriz, int linha, int coluna, int tamanho){
     return quantBombas++;
 }
 
-/*Função responsável por percorrer cada coordenada da matriz, e quando não há bomba no local 
-é chamada a função 'colocarQuantidadeDeBombas' para somar quantas bombas tem ao redor 
+/*Funcao responsavel por percorrer cada coordenada da matriz, e quando nao ha bomba no local 
+eh chamada a funcao 'colocarQuantidadeDeBombas' para somar quantas bombas tem ao redor 
 da coordenada*/
 void montarMatrizDoJogo(int **matriz,int tamanho){
    int i=0,j=0;
@@ -207,7 +207,7 @@ void montarMatrizDoJogo(int **matriz,int tamanho){
    }
 }
 
-/*Função responsável por modificar a matriz do jogador de acordo com as coordenadas selecionadas*/
+/*Funcao responsavel por modificar a matriz do jogador de acordo com as coordenadas selecionadas*/
 void modificar_campo_minado(int **matrizJogo,char **matrizUsuario,int n,int m,int *bomba){
 
    /*Caso a coordenada escolhida seja uma bomba, o ponteiro que aponta para a flagBomba 
@@ -218,29 +218,29 @@ void modificar_campo_minado(int **matrizJogo,char **matrizUsuario,int n,int m,in
    matrizUsuario[n][m]='B';
   }else{
 
-   /*Caso contrário, a coordenada selecionada pelo usuário recebe a quantidade de bombas vizinhas.
-   Recebida da matriz "original" do jogo, montada logo no início.*/
+   /*Caso contrario, a coordenada selecionada pelo usuario recebe a quantidade de bombas vizinhas.
+   Recebida da matriz "original" do jogo, montada logo no inicio.*/
    matrizUsuario[n][m]=matrizJogo[n][m]+'0';
    printf("\n\nUHU! Sem bomba por aqui!\n");
   }
 }
 
-/*Função verifica se a coordenda digitada pelo usuário existe*/
+/*Funcao verifica se a coordenada digitada pelo usuario existe*/
 int verificar_Coordenada_Existente(int linha, int coluna,int tam){
    int flagCoordenada=1;
 
-   //Verifica se a linha ou coluna são menores que 0 ou maiores que o tamanho da matriz do jogo
+   //Verifica se a linha ou coluna sao menores que 0 ou maiores que o tamanho da matriz do jogo
    if(linha>tam-1 || linha<0 || coluna>tam-1 || coluna<0){
       flagCoordenada=0;
    }
    return flagCoordenada;
 }
 
-/*Função verifica se a coordenada digitada pelo usuário é repetida.*/
+/*Funcao verifica se a coordenada digitada pelo usuário eh repetida.*/
 int verificar_Coordenada_Repetida(int linha, int coluna,int ** matriz_copia){
    int flagCoordenada=1;
 
-   /* Toda vez que uma nova coordenada é selecionada a matriz copia recebe 1, ou seja,
+   /* Toda vez que uma nova coordenada eh selecionada a matriz copia recebe 1, ou seja,
    quando o valor da matriz for diferente de 0 a coordenada ja foi selecionada. Retornando falso.*/
    if(matriz_copia[linha][coluna]!=0){
       flagCoordenada=0;
@@ -255,7 +255,7 @@ int verificar_Coordenada_Repetida(int linha, int coluna,int ** matriz_copia){
    printf("************ BEM VINDO(A) AO CAMPO MINADO! ************\n");
    printf("Para iniciar o jogo selecione o modo. Digite:\n1 - Facil\n2 - Medio\n3 - Dificil\n");
 
-   //Verifica se o usuário coloca o modo diferente de 1,2 e 3
+   //Verifica se o usuario coloca o modo diferente de 1,2 e 3
    while(scanf("%d",&modo),modo<1 || modo>3){
     printf("Modo Indisponivel. Digite:\n1 - Facil\n2 - Medio\n3 - Dificil\n");
    }
@@ -263,25 +263,25 @@ int verificar_Coordenada_Repetida(int linha, int coluna,int ** matriz_copia){
    //Configurar o campo minado de acordo com a dimensao da matriz e a quantidade de bombas
    dificuldadeJogo(&modo,&tam,&quantBombas);
 
-   /*Função para alocar e criar a matriz do jogo e em seguida inicializar ela*/
+   /*Funcao para alocar e criar a matriz do jogo e em seguida inicializar ela*/
    int **campo_minado= alocar_matriz(tam);
    inicializar_matriz(campo_minado,tam);
 
-   /*Função para alocar, criar e inicializar a matriz copia do jogo, será necessária 
-   posteriormente para tratar a execeção se o usuário digitar coordenadaas repetidas*/
+   /*Funcao para alocar, criar e inicializar a matriz copia do jogo, necessaria 
+   posteriormente para tratar a exececao se o usuario digitar coordenadaas repetidas*/
    int **copia_campo_minado= alocar_matriz(tam);
    inicializar_matriz(copia_campo_minado,tam);
 
-   /*Função para inserir as bombas de modo aleatório e em seguida montar a matriz do jogo
+   /*Funcao para inserir as bombas de modo aleatorio e em seguida montar a matriz do jogo
    calculando os vizinhos da bomba*/
    inserirBombas(quantBombas,campo_minado,tam);
    montarMatrizDoJogo(campo_minado,tam);
 
-   /*Função para alocar, criar e inicializar a matriz de vizualizção do usuário*/
+   /*Funcao para alocar, criar e inicializar a matriz de visualizacao do usuario*/
    char **campo_minado_usuario= alocarEinicializar_matriz_usuario(tam);
    
 
-   int quantJogadas=tam*tam-quantBombas; //calcula a quantidade de jogadas do usuário
+   int quantJogadas=tam*tam-quantBombas; //calcula a quantidade de jogadas do usuario
    while(cont<quantJogadas && flagbomba){
 
       printf("***********************************************\n");
@@ -293,19 +293,19 @@ int verificar_Coordenada_Repetida(int linha, int coluna,int ** matriz_copia){
       linha-=1;
       coluna-=1;
 
-      //Chama a função para verificar se a coordenada digitida pelo usuário existe
+      //Chama a funcao para verificar se a coordenada digitada pelo usuario existe
       if(verificar_Coordenada_Existente(linha, coluna,tam)==0){
          printf("\n\n-> COORDENADA NAO EXISTE. Digite novamente!\n");
       }else{
 
-         //Chama a função para verificar se a coordenada digitida pelo usuário é repetida
+         //Chama a funcao para verificar se a coordenada digitada pelo usuário eh repetida
          if(verificar_Coordenada_Repetida(linha, coluna,copia_campo_minado)){
 
-            //Todo nova coordenada selecionada pelo usuário a matriz copia recebe 1
+            //Todo nova coordenada selecionada pelo usuario a matriz copia recebe 1
             copia_campo_minado[linha][coluna]=1;
 
-            /*Quando verificada que a coordenada existe e não está repetida, irá modificar 
-            o campo minado do usuário*/
+            /*Quando verificada que a coordenada existe e nao esta repetida, ira modificar 
+            o campo minado do usuario*/
             modificar_campo_minado(campo_minado,campo_minado_usuario,linha,coluna,&flagbomba);
             cont++;
          }else{
@@ -317,7 +317,7 @@ int verificar_Coordenada_Repetida(int linha, int coluna,int ** matriz_copia){
      printf("\n");
    }
 
-   /*Quando encerrado as tentativas, verifica se o jogador ganhou ou perdeu, através da flagbomba.
+   /*Quando encerrado as tentativas, verifica se o jogador ganhou ou perdeu, atraves da flagbomba.
    Mostrando a matriz original do jogo.*/
    visualizar_matriz_usuario(campo_minado_usuario,tam);
    if(flagbomba==0){
